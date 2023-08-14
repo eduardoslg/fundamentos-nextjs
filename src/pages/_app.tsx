f78c6480ee94c2e0b2a99824578f7d661d74392f
eduardoslg <eduardo.seve@hotmail.com>
2023-08-14 15:11:37 -0300

    finalização do desafio ignite shop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,19 @@
-import { AppProps } from "next/app"
-import { globalStyles } from "../styles/global"
+import { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import { CartContextProvider } from "../contexts/CartContext";
+import { globalStyles } from "../styles/global";
 
-import logoImg from "../assets/logo.svg"
-import { Container, Header } from "../styles/pages/app"
+import { Container } from "../styles/pages/app";
 
-import Image from "next/future/image"
+globalStyles();
 
-globalStyles()
-
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
-
-      <Component {...pageProps} />
-    </Container>
-  )
+    <CartContextProvider>
+      <Container>
+        <Header />
+        <Component {...pageProps} />
+      </Container>
+    </CartContextProvider>
+  );
 }
-
-export default App
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,50 +1,39 @@
 import axios from "axios";
-import { GetStaticPaths, GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/future/image";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Stripe from "stripe";
+import { IProduct } from "../../contexts/CartContext";
+import { useCart } from "../../hooks/useCart";
 import { stripe } from "../../lib/stripe";
-import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 interface ProductProps {
-  product: {
-    id: string
-    name: string
-    imageUrl: string
-    price: string
-    description: string
-    defaultPriceId: string
-  }
+  product: IProduct;
 }
 
 export default function Product({ product }: ProductProps) {
-  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+  const { isFallback } = useRouter();
 
-  async function handleBuyButton() {
-    try {
-      setIsCreatingCheckoutSession(true);
+  const { addToCart, checkIfItemAlreadyExists } = useCart();
 
-      const response = await axios.post('/api/checkout', {
-        priceId: product.defaultPriceId,
-      })
-
-      const { checkoutUrl } = response.data;
-
-      window.location.href = checkoutUrl;
-    } catch (err) {
-      setIsCreatingCheckoutSession(false);
-
-      alert('Falha ao redirecionar ao checkout!')
-    }
+  if (isFallback) {
+    return <p>Loading...</p>;
   }
+
+  const itemAlreadyInCart = checkIfItemAlreadyExists(product.id);
 
   return (
     <>
       <Head>
-        <title>{product.name} | Ignite Shop</title>
+        <title>{`${product.name} | Ignite Shop`}</title>
       </Head>
-
       <ProductContainer>
         <ImageContainer>
           <Image src={product.imageUrl} width={520} height={480} alt="" />
@@ -56,29 +45,36 @@ export default function Product({ product }: ProductProps) {
 
           <p>{product.description}</p>
 
-          <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
-            Comprar agora
+          <button
+            disabled={itemAlreadyInCart}
+            onClick={() => addToCart(product)}
+          >
+            {itemAlreadyInCart
+              ? "Produto já está no carrinho"
+              : "Colocar na sacola"}
           </button>
         </ProductDetails>
       </ProductContainer>
     </>
-  )
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [
-      { params: { id: 'prod_MLH5Wy0Y97hDAC' } },
-    ],
-    fallback: 'blocking',
-  }
-}
+  const paths = [{ params: { id: "prod_MO0C3yNu4UBp9p" } }];
 
-export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
   const productId = params.id;
 
   const product = await stripe.products.retrieve(productId, {
-    expand: ['default_price']
+    expand: ["default_price"],
   });
 
   const price = product.default_price as Stripe.Price;
@@ -89,14 +85,15 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
         id: product.id,
         name: product.name,
         imageUrl: product.images[0],
-        price: new Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL'
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
         }).format(price.unit_amount / 100),
+        numberPrice: price.unit_amount / 100,
         description: product.description,
-        defaultPriceId: price.id
-      }
+        defaultPriceId: price.id,
+      },
     },
-    revalidate: 60 * 60 * 1 // 1 hours
-  }
-}
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
